640229bfb3bb6a52f7a5f037b7960179e18d7e78
Nicolas <nicolasspragg@gmail.com>
2017-11-08 21:05:20 -0500

    added new storyboard view + image assets

---
+++ 490test/490test/MainMenu.swift
@@ -0,0 +1,32 @@
+//
+//  MainMenu.swift
+//  490test
+//
+//  Created by Nicolas Spragg on 2017-11-08.
+//  Copyright © 2017 TBD. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainMenu: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
