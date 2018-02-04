beb7bc52ef43d1949ce87272bd89707963d0a56d
Nicolas <nicolasspragg@gmail.com>
2018-02-03 23:35:01 -0500

    removed print

--- 490test/490test/Classes/InProgressWorkoutManager.swift
+++ 490test/490test/Classes/InProgressWorkoutManager.swift
@@ -34,9 +34,9 @@ class InProgessWorkoutManager {
         // return true or false if they are on track to meet workout goals
         var timeElapsed = totalDistArray.count * 5
         var currentSpeed = Double(totalDistArray.last!) / Double (timeElapsed)
-        print(timeElapsed)
-        print(currentSpeed)
-        print(speedGoal)
+       // print(timeElapsed)
+       // print(currentSpeed)
+     //   print(speedGoal)
         if (currentSpeed >= speedGoal) {
             return true
         }
